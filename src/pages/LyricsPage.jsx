0749c923fbc98b11067146b7381dd911aa926318
Brandon Stilson <bbstilson@gmail.com>
2016-05-23 16:12:42 -0700

    added page styles to LyricsPage

--- src/pages/LyricsPage.jsx
+++ src/pages/LyricsPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Header, Footer } from 'components'
 import { LyricsContainer } from 'containers'
+import { page as styles } from 'styles/global'
 
 export default (props) => (
-    <div>
+    <div style={styles}>
         <Header />
         <LyricsContainer location={props.location} />
         <Footer />
